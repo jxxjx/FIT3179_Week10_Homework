91b6e52d4f9b33c95babedd23100d3ceb3a92403
jingxuan <140064548+jxxjx@users.noreply.github.com>
2025-10-07 20:27:46 +0800

    add line chart

--- javascript.js
+++ javascript.js
@@ -2,3 +2,8 @@ var vg_1 = "arrivals_symbol_map.json";
 vegaEmbed("#symbol_map", vg_1).then(function(result) {
 }).catch(console.error);
 
+
+
+var vg_2 = "arrivals_bar_chart.json";
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
+}).catch(console.error);
